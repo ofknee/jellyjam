f4ee427a425ca72995038bc26b28d65b287e464b
PixelSaver <stephen.k@proton.me>
2026-02-09 21:07:26 -0800

    random bs

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'jelly-pink': '#ff71ce',
+				'jelly-blue': '#01cdfe',
+			},
+		},
+	},
+	plugins: [],
+}
